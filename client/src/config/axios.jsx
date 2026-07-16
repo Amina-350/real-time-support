7adcc838f536c33355a1f0d618d390160867d768
Amina Arshad <aminaarshad350@gmail.com>
2026-07-16 22:49:28 +0500

    Fxing

--- client/src/config/axios.jsx
+++ client/src/config/axios.jsx
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+console.log("Backend URL:", process.env.REACT_APP_BACKEND_URL);
+
 const api = axios.create({
-  baseURL:
-    process.env.REACT_APP_BACKEND_URL || "http://localhost:4000/api",
+  baseURL: `${process.env.REACT_APP_BACKEND_URL}/api`,
 });
+
+console.log("Axios Base URL:", api.defaults.baseURL);
 
 export default api;
